0e0a413926af6a4634aea78aae790a0ccf437555
Anik Raj <anikraj1994@gmail.com>
2017-02-07 22:51:21 +0530

    added sw

--- js/main.js
+++ js/main.js
@@ -61,3 +61,15 @@ function filterApply(object) {
         holder.appendChild(clone);
     });
 }
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+        navigator.serviceWorker.register('/sw.js').then(function(registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }).catch(function(err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
