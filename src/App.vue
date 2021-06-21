8f8bd61cf88237363a2dd90bc6f43e0c9baa5c7a
Anik Raj <anikraj1994@gmail.com>
2021-06-21 21:57:50 +0530

    fixed sw

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home4</router-link> |
+      <router-link to="/">Home5</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view />
